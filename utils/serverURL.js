f4f441f87b64b4344ef45dd60752cfb7266943dd
emanuelefavero <emafaux89@gmail.com>
2023-02-01 16:40:38 +0100

    add Vercel URL to utils/serverURL

--- utils/serverURL.js
+++ utils/serverURL.js
@@ -3,4 +3,4 @@ const dev = process.env.NODE_ENV !== 'production'
 // TODO - change this to your deployed vercel URL
 export const serverURL = dev
   ? 'http://localhost:3000'
-  : 'YOUR_DEPLOYED_VERCEL_URL'
+  : 'https://nextjs-mongoose-tau.vercel.app/'
